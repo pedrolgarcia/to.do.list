9f23d48b15bdec8f1b412f99c08b4f9a01bacc82
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-30 01:30:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+
+import "./global.css";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
